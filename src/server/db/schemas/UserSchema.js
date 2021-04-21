bf234a7353309d164bf20a1e94b9014bc5d6c220
n.mihajlovic <n.mihajlovic@frontdeskanywhere.com>
2021-04-21 16:22:54 +0200

    Added inital options to schema

--- src/server/db/schemas/UserSchema.js
+++ src/server/db/schemas/UserSchema.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-  firstName: { type: String },//, required: true, trim: true },
-  lastName: { type: String },// required: true, trim: true },
-  username: { type: String },// required: true, trim: true, unique: true },
-  email: { type: String },// required: true, trim: true, unique: true },
-  password: { type: String },// required: true },
-  profilePic: { type: String },// default: '/images/profilePic.png' }
-})
+  firstName: { type: String, required: true, trim: true },
+  lastName: { type: String, required: true, trim: true },
+  username: { type: String, required: true, trim: true, unique: true },
+  email: { type: String, required: true, trim: true, unique: true },
+  password: { type: String, required: true },
+  profilePic: { type: String, default: '/images/profilePic.png' }
+}, { timestamps: true })
 const UserModel = mongoose.model("User", UserSchema)
 
 module.exports = UserModel
