910b781de056b13742cfca44ed4e3d75a18a1d08
n.mihajlovic <n.mihajlovic@frontdeskanywhere.com>
2021-04-27 19:14:18 +0200

    Added client js for enabling and disabling form

--- src/app/src/js/index.js
+++ src/app/src/js/index.js
@@ -1,1 +1,27 @@
-console.log("TESTIRAM INDEX")
+document.querySelector('textarea#post')
+  .addEventListener('keyup', function (e) {
+    const postValue = e.target.value;
+    const postBtn = document.querySelector('button#submitPostButton')
+
+    if (postValue.trim().length == 0) {
+      disableButton(postBtn)
+    } else {
+      enableButton(postBtn)
+    }
+
+    postBtn.disabled = postValue.trim().length == 0
+  })
+
+function enableButton(postBtn) {
+  postBtn.classList.add('hover:bg-brand-purple-hover');
+  postBtn.classList.add('cursor-pointer');
+  postBtn.classList.remove('cursor-auto');
+  postBtn.classList.remove('bg-opacity-50');
+}
+
+function disableButton(postBtn) {
+  postBtn.classList.add('bg-opacity-50');
+  postBtn.classList.add('cursor-auto');
+  postBtn.classList.remove('hover:bg-brand-purple-hover');
+  postBtn.classList.remove('cursor-pointer');
+}
