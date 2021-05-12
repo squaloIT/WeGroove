d1509c680d9689e592afc4cc30cb05443bd071cb
n.mihajlovic <n.mihajlovic@frontdeskanywhere.com>
2021-05-12 12:08:06 +0200

    Uncommented session check in isLoggedIn middleware

--- src/server/middleware.js
+++ src/server/middleware.js
@@ -1,6 +1,5 @@
 exports.checkIsLoggedIn = (req, res, next) => {
-  if (true) {
-    // if (req.session && req.session.user) {
+  if (req.session && req.session.user) {
     return next();
   } else {
     return res.redirect('/login');
