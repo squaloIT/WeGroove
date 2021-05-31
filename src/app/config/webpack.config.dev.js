c4269477ed62953b5eeab76cc96736b1a0d16d86
n.mihajlovic <n.mihajlovic@frontdeskanywhere.com>
2021-05-31 20:03:41 +0200

    deleted wathc external files webpack plugin

--- src/app/config/webpack.config.dev.js
+++ src/app/config/webpack.config.dev.js
@@ -2,7 +2,6 @@ const path = require('path');
 const webpack = require('webpack')
 const Dotenv = require('dotenv-webpack');
 const CopyPlugin = require('copy-webpack-plugin');
-const WatchExternalFilesPlugin = require('webpack-watch-files-plugin').default;
 
 module.exports = {
   mode: 'development',
@@ -56,11 +55,6 @@ module.exports = {
           from: path.resolve(__dirname, './../src/assets'),
           to: path.resolve(__dirname, './../dist/assets'),
         }
-      ]
-    }),
-    new WatchExternalFilesPlugin({
-      files: [
-        path.resolve(__dirname, './../../server/templates/views/*'),
       ]
     })
   ],
