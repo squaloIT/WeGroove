d409c46851bd1cbc114346e7e000af16c85b5141
n.mihajlovic <n.mihajlovic@frontdeskanywhere.com>
2021-06-04 11:41:59 +0200

    Added coverPic to schema

--- src/server/db/schemas/UserSchema.js
+++ src/server/db/schemas/UserSchema.js
@@ -8,7 +8,8 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, trim: true, unique: true },
   email: { type: String, required: true, trim: true, unique: true },
   password: { type: String, required: true },
-  profilePic: { type: String, default: '/images/profilePic.jpg' },
+  profilePic: { type: String, default: '/assets/profilePic.jpg' },
+  coverPic: { type: String, default: '/assets/coverPic.jpg' },
   likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
   retweets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
 }, { timestamps: true });
