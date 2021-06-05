a10f75dde17ab3608fbe84c1a9594fac77c24232
n.mihajlovic <n.mihajlovic@frontdeskanywhere.com>
2021-06-05 18:16:14 +0200

    profile page and their tabs changed to use backend data fetching and rendering

--- src/server/routes/profileRouter.js
+++ src/server/routes/profileRouter.js
@@ -21,6 +21,30 @@ router.get('/:username', async (req, res, next) => {
     page: 'profile',
     title: user.username,
     posts: allUserPosts,
+    active: "Posts",
+    user
+  })
+})
+
+router.get('/:username/:tab', async (req, res, next) => {
+  const user = await UserModel.findByUsernameOrID(req.params.username)
+    .catch(err => {
+      console.log(err);
+      return res.redirect('/')
+    })
+
+  /** @type { post } */
+  const allUserPosts = await PostModel.findAllUserPosts(user._id, req.params.tab.toLowerCase())
+    .catch(err => {
+      console.log(err);
+      return res.redirect('/')
+    })
+
+  res.render('main', {
+    page: 'profile',
+    title: user.username,
+    posts: allUserPosts,
+    active: req.params.tab,
     user
   })
 })
