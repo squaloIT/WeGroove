741379383b8f9fd9451528115c19858794a40e55
n.mihajlovic <n.mihajlovic@frontdeskanywhere.com>
2021-06-19 16:25:15 +0200

    creating chat if userId is passed in URL

--- src/server/routes/messageRouter.js
+++ src/server/routes/messageRouter.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const { Schema } = require('mongoose');
+const ChatModel = require('../db/schemas/ChatSchema');
 const ChatSchema = require('../db/schemas/ChatSchema');
+const UserModel = require('../db/schemas/UserSchema');
 const router = express.Router();
 
 require('./../typedefs');
@@ -20,6 +23,7 @@ router.get('/', async (req, res, next) => {
   res.status(200).render('main', {
     title: "Inbox",
     page: 'inbox',
+    subPage: 'inbox',
     user: req.session.user,
     chats: chatWihoutLoggedUser
   });
@@ -28,9 +32,66 @@ router.get('/', async (req, res, next) => {
 router.get('/new', (req, res, next) => {
   res.status(200).render('main', {
     title: "New Message",
+    subPage: 'new-message',
     page: 'inbox',
     user: req.session.user
   });
 })
+
+router.get('/:chatId', async (req, res, next) => {
+  const chatId = req.params.chatId;
+  const userId = req.session.user._id;
+
+  var chat = await ChatModel.findOne({
+    _id: chatId,
+    users: { $elemMatch: { $eq: userId } }
+  })
+
+  if (chat == null) {
+    const userFound = await UserModel.findById(chatId);
+
+    if (userFound == null) {
+      return res.redirect('/')
+    } else {
+      chat = await createOrGetChatWithUser(userFound, req.session.user);
+    }
+  }
+
+  res.status(200).render('main', {
+    title: "Chat",
+    subPage: 'chat',
+    page: 'inbox',
+    user: req.session.user,
+    chat
+  });
+})
+
+function createOrGetChatWithUser(userFound, userLogged) {
+  return ChatModel.findOneAndUpdate(
+    {
+      isGroupChat: false,
+      users: [userFound._id, userLogged._id]
+      // users: {
+      //   $size: 2,
+      //   $all: [
+      //     {
+      //       $elemMatch: { $eq: Schema.Types.ObjectId(userFound._id) },
+      //       $elemMatch: { $eq: Schema.Types.ObjectId(userLogged._id) }
+      //     }
+      //   ]
+      // }
+    },
+    {
+      $setOnInsert: {
+        users: [userFound._id, userLogged._id]
+      }
+    },
+    {
+      new: true,
+      upsert: true
+    }
+  )
+    .populate('users')
+}
 
 module.exports = router;
