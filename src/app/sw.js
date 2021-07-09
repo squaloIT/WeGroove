f44f7ba63026d7b180cea10eb6c5e81635ae6a0a
n.mihajlovic <n.mihajlovic@frontdeskanywhere.com>
2021-07-09 11:28:23 +0200

    added some workbox recipes and added background sync for /api/*

--- src/app/sw.js
+++ src/app/sw.js
@@ -1,17 +1,18 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
-import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { BackgroundSyncPlugin } from 'workbox-background-sync';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute } from 'workbox-precaching';
+import { googleFontsCache, imageCache, staticResourceCache } from 'workbox-recipes';
 import { registerRoute } from 'workbox-routing';
-import { CacheFirst, NetworkOnly, StaleWhileRevalidate } from 'workbox-strategies';
+import { CacheFirst, NetworkFirst, NetworkOnly, StaleWhileRevalidate } from 'workbox-strategies';
 
 workbox.setConfig({ debug: true });
 // workbox.skipWaiting();
 // workbox.precaching.precacheAndRoute(self.__WB_MANIFEST)
 
 precacheAndRoute([
-  { url: '/', revision: 'dasdasczxczxc2323' },
+  // { url: '/', revision: 'dasdasczxczxc2323' }, - Ne moze da bude ovde jer onda dodavanje novog posta ne bude procitano. 
   { url: '/script.bundle.js', revision: '123123asdasdasd' },
   { url: '/src_js_index_js.bundle.js', revision: 'asdasdasd' },
   { url: '/assets/coverPic.jpg', revision: '2e12ue1u2en' },
@@ -30,31 +31,8 @@ precacheAndRoute([
   { url: 'https://cdn.jsdelivr.net/npm/emoji-picker-element-data@%5E1/en/emojibase/data.json', revision: null },
 ])
 
-registerRoute(
-  ({ request, url }) => request.destination === 'image' || url.pathname.startsWith('/uploads/'),
-  new StaleWhileRevalidate({
-    cacheName: 'image-cache',
-    plugins: [
-      new ExpirationPlugin({
-        maxEntries: 100,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-      }),
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      })
-    ]
-  })
-);
-
-registerRoute(
-  ({ request }) => request.destination === 'script' || request.destination === 'style',
-  new StaleWhileRevalidate()
-);
-
-registerRoute(
-  ({ url }) => url.pathname.startsWith('/socket.io/'),
-  new NetworkOnly()
-);
+imageCache()
+staticResourceCache()
 
 registerRoute(
   ({ url }) => url.hostname === 'fonts.gstatic.com' || url.hostname === 'ka-f.fontawesome.com',
@@ -62,9 +40,15 @@ registerRoute(
     cacheName: 'font-awesome-cache'
   }),
 );
+googleFontsCache()
 
 registerRoute(
-  ({ request, sameOrigin }) => sameOrigin && request.destination === 'document',
+  ({ url }) => url.pathname.startsWith('/socket.io/'),
+  new NetworkOnly()
+);
+
+registerRoute(
+  ({ request, sameOrigin, url }) => sameOrigin && request.destination === 'document' && !isInNetworkFirstList(request, url),
   new StaleWhileRevalidate({
     cacheName: 'documents',
     plugins: [
@@ -74,4 +58,43 @@ registerRoute(
       }),
     ]
   }),
+
 );
+
+const bgSyncPlugin = new BackgroundSyncPlugin('myQueueName', {
+  onSync: function (options) {
+    console.log("[Service worker]: after sync options, ", options)
+  },
+  maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
+});
+
+registerRoute(
+  ({ url }) => url.pathname.startsWith('/api/'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+    // networkTimeoutSeconds: 3
+  }),
+  'POST'
+);
+
+registerRoute(
+  ({ url }) => url.pathname.startsWith('/api/'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+    // networkTimeoutSeconds: 3
+  }),
+  'PUT'
+);
+
+registerRoute(
+  ({ request, url }) => isInNetworkFirstList(request, url),
+  new NetworkFirst({
+    cacheName: "network-first-cache-later",
+    networkTimeoutSeconds: 3
+  })
+)
+function isInNetworkFirstList(request, url) {
+  console.log(url.pathname)
+  console.log(url.pathname.startsWith('/messages'))
+  return url.pathname.startsWith('/messages')
+}
