37b50f6c408d3a740da86e1090c59aecf28b60d1
n.mihajlovic <n.mihajlovic@frontdeskanywhere.com>
2021-08-31 12:14:27 +0200

    feat: displaying videos and on click changing the main one

--- src/app/src/js/call_room.js
+++ src/app/src/js/call_room.js
@@ -19,20 +19,24 @@ export default function call_room() {
   }).then(stream => {
     addMyVideoStreaming(myVideo, stream)
 
+    const leaveCallButton = document.querySelector('button#leave-call');
+    leaveCallButton.addEventListener('click', function () {
+      window.location.href = '/'
+    })
+
     myPeer.on('call', call => {
-      console.log("ON CALL")
       call.answer(stream)
       const video = document.createElement('video')
+
       call.on('stream', userVideoStream => {
         addVideoStream(video, userVideoStream)
       })
     })
 
     socket.on('user-connected-to-call', userId => {
-
       setTimeout(() => {
         connectToNewUser(userId, stream)
-      }, 4000)
+      }, 2000)
     })
   })
 
@@ -62,19 +66,31 @@ export default function call_room() {
    * @param {HTMLElement} video 
    * @param {*} stream 
    */
+  let timeout = null;
   function addVideoStream(video, stream) {
+    console.log("POZVAN addVideoStream")
     const videoGrid = document.getElementById('video-grid')
 
     video.srcObject = stream
-    video.classList.add("mr-4")
-    video.classList.add("w-2/5")
-    // video.classList.add("xs:w-1/2")
-    video.classList.add("lg:w-1/4")
     video.classList.add("object-cover")
+    video.classList.add("cursor-pointer")
+    video.classList.add("hover:border")
+    video.classList.add("hover:border-white")
+    video.classList.add("max-h-80")
+    video.classList.add("mb-4")
 
     video.addEventListener('loadedmetadata', () => {
       video.play()
     })
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      video.addEventListener('click', () => {
+        const bigVideo = document.querySelector('#big-video-wrapper video')
+        const smallVideoSrcObject = video.srcObject;
+        video.srcObject = bigVideo.srcObject;
+        bigVideo.srcObject = smallVideoSrcObject;
+      })
+    }, 1000)
     videoGrid.append(video)
   }
   /**
@@ -88,6 +104,7 @@ export default function call_room() {
     video.srcObject = stream
     video.classList.add("h-full")
     video.classList.add("w-full")
+    video.classList.add("object-cover")
 
     video.addEventListener('loadedmetadata', () => {
       video.play()
