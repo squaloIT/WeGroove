305cef4a9d2506d905d788ec55ca1e570c9a173c
n.mihajlovic <n.mihajlovic@frontdeskanywhere.com>
2021-09-14 16:10:17 +0200

    ready for heroku

--- src/app/config/webpack.config.prod.js
+++ src/app/config/webpack.config.prod.js
@@ -58,9 +58,9 @@ module.exports = {
         // },
       ]
     }),
-    new Workbox.InjectManifest({
-      swSrc: path.resolve(__dirname, './../sw.js'),
-      swDest: 'service-worker.js',
-    })
+    // new Workbox.InjectManifest({
+    //   swSrc: path.resolve(__dirname, './../sw.js'),
+    //   swDest: 'service-worker.js',
+    // })
   ],
 };
