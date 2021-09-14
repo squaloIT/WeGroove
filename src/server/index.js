0cf7d59cefc784a914796b5d69ef96697eba0c79
n.mihajlovic <n.mihajlovic@frontdeskanywhere.com>
2021-09-14 16:04:26 +0200

    feat: helmet added to server

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 var { Liquid } = require('liquidjs');
 const path = require('path')
+const helmet = require('helmet')
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
@@ -18,7 +19,6 @@ connect(server);
 const peerServer = PeerServer({ port: 9000, path: '/' });
 
 const viewsPath = path.join(__dirname, "./templates/views")
-const partialsPath = path.join(__dirname, "./templates/partials")
 
 console.log(`Is in production ${process.env.NODE_ENV === 'production'}`)
 var engine = new Liquid({
@@ -40,6 +40,22 @@ app.use(session({
   saveUninitialized: false
 }));
 
+app.use(helmet({
+  contentSecurityPolicy: false
+}))
+//* Referrer-Policy - ja sam definisao no refferer kao vrednost jer ne zelim da se prosledjuje do drugog sajta url adresa mog sajta i info o tome da je user dosao sa mog sajta na njegov
+
+//* Strict-Transport-Security - govori browseru da prefer-uje je https over http, i koliko dugo da pamti https konekcije
+
+//* X-Content-Type-Options - default je no sniff. Ovaj Header govori browseru da ne treba da nagadja mime type resursa koji je stigao sa servera, vec da striktno koristi ono sto ce se poslati u content-type. Ako ovo postavim, i recimo za html stranicu na serveru kazem da ima content-type text, ona se nece renderovati kako treba iako je .html fajl.
+
+//* X-DNS-Prefetch-Control - response header koji kontrolise DNS prefetching. Ovaj feature omogucava browseru da razresava sve url adrese u ip adrese koje user moze da klikne dok je on na stranici, ovo omogucava korisniku da brze stigne do neke stranice! Ukoliko ovo iskljucim kao sto sam ja kod mene iskljucio onda sam se zastitio od toga da neko pogresno i zlonamerno promeni neku url adresu u ip adresu, naravno po cenu brzine jer ce tek kada se klikne na link razresiti url.
+
+//* X-Frame-Options - response header kojim se iframe moze ucitati samo sa odredjenih sajtova. 
+
+//* X-Powered-By - sklanja ovaj response header koji govori koji server je u pozadini. Ovo nema nikakvih security benefita, jer svakako onaj ko zeli moze da sazna koji je server u pozadini, vec jednostavno sklanja header koji nije bitan.
+
+//* X-XSS-Protection - 
 
 app.use('/api/posts', checkIsLoggedIn, postAPI);
 app.use('/api/chat', checkIsLoggedIn, chatAPI);
